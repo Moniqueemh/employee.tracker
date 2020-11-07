01217f029fa119345135b88be0eb73c6327c9bdf
Moniqueemh <mohernandez9992@yahoo.com>
2020-11-06 21:27:52 -0800

    prompt what would you like to do?

--- server.js
+++ server.js
@@ -1,11 +1,43 @@
-const mysql = requestAnimationFrame("mysql");
+const mysql = require("mysql");
 const inquirer = require("inquirer");
 
 let connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "Jackjack0307", 
+    password: "Jackjack0307!",
     database: "employee_DB"
 
-})
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadID);
+    main()
+});
+
+function main() {
+    connection.query("SELECT * FROM employee_db.department", function (err, res) {
+        if (err) throw err;
+        console.log(res);
+    });
+};
+
+function main() {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "choice",
+            message: "What would you like to do?",
+            choices: [
+                "Add Department?",
+                "Add Role?",
+                "Add Employee?",
+                "View by Department?",
+                "View by Role?",
+                "View by Employee?",
+                "Update Employee Role?"
+            ]
+        }
+    ])
+};
